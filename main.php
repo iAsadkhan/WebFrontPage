a204f3b7383aea16e4d2193ce18ebb8daf361357
iAsadkhan <iasadkhan0@gmail.com>
2020-07-23 14:57:11 +0500

    page1

---
+++ main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=Div, initial-scale=1.0">
+    <link rel = "icon" href = "images\LOGOF.png" type = "image/x-icon">
+    <link rel="stylesheet" href="stylesheet\main.php">
+    <title>TechNoob</title>
+</head>
+
+<body>
+    <div class="header-nav">
+        <nav>
+            <a href="main.php" img><img  src="images\LOGOF.png" alt="lol" width="100" height="90"></a>
+            <ul>
+                <li><a href="main.php">Home</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="aboutus.php">About</a></li>
+            </ul>
+        </nav>
+        <div class="text">
+            <h1>TechNoob</h1>
+            <p>The Tech Enthusiasts</p>
+            <button class="button" onclick="window.location.href = 'login.php';">Login</button>
+            <button class="button" onclick="window.location.href = 'signup.php';">Sign up</button>
+        </div>    
+    </div>
+</body>
+</html>
